169d931c05d47b0fbf77a7f277c1bd751888ddd7
shawgav <shawgav@sheridancollege.ca>
2022-01-18 13:42:16 -0500

    first commit

---
+++ src/studentwin22/Student.java
@@ -0,0 +1,14 @@
+package studentwin22;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shawg
+ */
+public class Student {
+    
+}
---
+++ src/studentwin22/Student_win22_tue_PM.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentwin22;
+
+/**
+ *
+ * @author shawg
+ */
+public class Student_win22_tue_PM {
+    
+}
